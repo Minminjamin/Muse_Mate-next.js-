d39a80d4d92d6cdb8da231e258769a188001a72f
Minminjamin <0429el@gmail.com>
2023-07-26 19:41:04 +0900

    feature : profile img component

---
+++ components/Atoms/Profile/Img.tsx
@@ -0,0 +1,33 @@
+import { GetServerSideProps } from "next";
+import Image from "next/image";
+import styles from "../../Style/Profile.module.css";
+
+// 부모 컴포넌트에서 props 받아오기
+// 일시적으로 any 선언, 추후 변경
+const Img = () => {
+  const profileImg = require("../../../public/basic img.jpg");
+
+  return (
+    <div>
+      <Image
+        src={profileImg}
+        alt="user profile"
+        width={54}
+        height={54}
+        className={styles.img}
+      />
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // const userId = context.query.userId;
+  // const profileImg = 0;
+  return {
+    props: {
+      //   profileImg,
+    },
+  };
+};
+
+export default Img;
