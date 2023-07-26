2e70318657cc1b397dd3162a12d6e8438168549e
Minminjamin <0429el@gmail.com>
2023-07-26 21:46:14 +0900

    style

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,15 @@
-import Name from "@/components/Atoms/Profile/Name";
-import { Html, Head, Main, NextScript } from "next/document";
-import KakaoLoginn from "./Login/KakaoLogin";
-import SideBar from "./SideBar/SideBar";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        {/* <Main />
-        <NextScript /> */}
-        <SideBar />
-        <Name />
-        <KakaoLoginn />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
