ab43e36be22e6ce5ed0b2a94be526494910e5ba5
Minminjamin <0429el@gmail.com>
2023-07-28 14:23:59 +0900

    refact

--- pages/api/auth/SignUp.ts
+++ pages/api/auth/SignUp.ts
@@ -9,7 +9,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if ((req.method = "POST")) {
+  if (req.method === "POST") {
     const { email, password, loginMethod, ...restInfo } = req.body;
 
     const client = await MongoClient.connect(uri);
