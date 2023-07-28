23452145162926885482dafadf0a7dea8d86f557
Minminjamin <0429el@gmail.com>
2023-07-28 13:34:42 +0900

    feature : next auth 셋팅

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,21 @@
 import NextAuth from "next-auth";
 import KakaoProvider from "next-auth/providers/kakao";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { MongoClient } from "mongodb";
+
+if (!process.env.NEXT_PUBLIC_MONGO_URI) throw new Error("env error");
+const uri: string = process.env.NEXT_PUBLIC_MONGO_URI;
 
 export default NextAuth({
   providers: [
     KakaoProvider({
       clientId: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID!,
       clientSecret: process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
     }),
   ],
 });
