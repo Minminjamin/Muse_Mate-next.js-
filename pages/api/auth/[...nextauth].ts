1d8d6bc9cfa0144607db13ec8835a8bc8aa764bc
Minminjamin <0429el@gmail.com>
2023-08-12 11:37:02 +0900

    feature : 자료형 변경

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,6 +5,9 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+// https://velog.io/@gonggi_bab/Next-Auth-%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-%ED%9A%8C%EC%9B%90%EA%B0%80%EC%9E%85-%EB%A1%9C%EA%B7%B8%EC%9D%B8
+// https://medium.com/prisma-korea/%EC%8B%A4%EC%9A%A9%EC%A0%81%EC%9D%B8-prisma-%EC%98%88%EC%A0%9C-5ad2bd13768f
+
 export const authOptions: NextAuthOptions = {
   providers: [
     KakaoProvider({
@@ -25,16 +28,21 @@ export const authOptions: NextAuthOptions = {
         user.email = profile.email || user.email;
       }
 
+      // 고유한 값으로 단일 사용자 레코드를 검색
       try {
         let db_user = await prisma.user.findUnique({
           where: { email: user.email || "" },
         });
 
+        // 새로운 User 레코드를 생성하는 쿼리 작성
         if (!db_user) {
           db_user = await prisma.user.create({
             data: {
+              id: "",
               name: user.name!,
               email: user.email!,
+              profile_img: "",
+
               // cart: {
               //   create: {
               //     profile: user.profile!,
