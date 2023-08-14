fc3ef9543f586537870944d85f37d0db1b3f95d3
Minminjamin <0429el@gmail.com>
2023-08-14 10:36:23 +0900

    feature : 회원가입 시 기본 프로필 저장 기능

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,9 +2,19 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import KakaoProvider from "next-auth/providers/kakao";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaClient } from "@prisma/client";
+import { v4 } from "uuid";
+import { v2 as cloudinary } from "cloudinary";
+// import v4 from "uuid"
 
+// https://cloudinary.com/documentation/node_integration#node_js_sdk_features
 const prisma = new PrismaClient();
 
+cloudinary.config({
+  cloud_name: "musemate",
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET_KEY,
+  secure: true,
+});
 // https://velog.io/@gonggi_bab/Next-Auth-%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-%ED%9A%8C%EC%9B%90%EA%B0%80%EC%9E%85-%EB%A1%9C%EA%B7%B8%EC%9D%B8
 // https://medium.com/prisma-korea/%EC%8B%A4%EC%9A%A9%EC%A0%81%EC%9D%B8-prisma-%EC%98%88%EC%A0%9C-5ad2bd13768f
 
@@ -34,14 +44,17 @@ export const authOptions: NextAuthOptions = {
           where: { email: user.email || "" },
         });
 
+        const basicProfileImg = require("../../../public/basic img.jpg");
         // 새로운 User 레코드를 생성하는 쿼리 작성
         if (!db_user) {
+          const uuid = v4();
+
           db_user = await prisma.user.create({
             data: {
-              id: "",
+              id: uuid,
               name: user.name!,
               email: user.email!,
-              profile_img: "image-url",
+              profile_img: "",
 
               // cart: {
               //   create: {
@@ -53,6 +66,15 @@ export const authOptions: NextAuthOptions = {
             },
           });
         }
+
+        const imgData = cloudinary.uploader.upload(basicProfileImg);
+        const imgDataUrl = (await imgData).url;
+
+        db_user = await prisma.user.update({
+          where: { email: user.email || "" },
+          data: { profile_img: imgDataUrl },
+        });
+
         user.id = db_user.id;
         // user.role = db_user.role;
 
