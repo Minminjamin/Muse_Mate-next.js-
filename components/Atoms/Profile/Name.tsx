2ec29bda32788a14744ca0f8d91560b6617c90c6
Minminjamin <0429el@gmail.com>
2023-08-19 14:45:24 +0900

    feature : 로그인한 user 이름 출력

--- components/Atoms/Profile/Name.tsx
+++ components/Atoms/Profile/Name.tsx
@@ -2,12 +2,13 @@ import { GetServerSideProps } from "next";
 import { useEffect } from "react";
 import styles from "../../Style/Profile.module.css";
 // 부모 컴포넌트나 커스텀 훅으로 데이터 받을 수 있게 설정
-const Name = () => {
-  const name: string = "jamin";
+const Name = ({ user }: { user: any }) => {
+  // const name: string = "jamin";
 
+  // const { user } = props;
   return (
     <div>
-      <h3 className={styles.name}>{name}</h3>
+      <h3 className={styles.name}>{user.name}</h3>
     </div>
   );
 };
