2d214c1e8f7bac39a6f73c8cbc8bc99eaca82ed2
Minminjamin <0429el@gmail.com>
2023-08-21 20:02:16 +0900

    feature  : 메인 화면으로 이동

--- components/Molecules/SideBar/HomeMenu.tsx
+++ components/Molecules/SideBar/HomeMenu.tsx
@@ -5,14 +5,21 @@ import { IoMdMusicalNotes, IoMdHeadset } from "react-icons/io";
 import { BsMic } from "react-icons/bs";
 import { IconType } from "react-icons";
 import styles from "../../Style/List.module.css";
+import { useRouter } from "next/router";
 
 const HomeMenu = () => {
+  const router = useRouter();
+
   const homeMenu: string[] = ["DISCOVER", "GENRE", "TOP CHARTS", "PODCAST"];
   const homeIcon = [BiSearchAlt, IoMdMusicalNotes, IoMdHeadset, BsMic];
 
+  const onHandleHome = () => {
+    router.push("/");
+  };
+
   return (
     <ul className={styles.list}>
-      <h2>HOME</h2>
+      <h2 onClick={onHandleHome}>HOME</h2>
       {homeMenu.map((item, index) => (
         <List menu={item} icon={homeIcon[index]} key={index} />
       ))}
