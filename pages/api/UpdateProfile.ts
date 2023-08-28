558afe687f86f12bcadf454c5dd2a558c25104ab
Minminjamin <0429el@gmail.com>
2023-08-28 23:47:32 +0900

    feature : name, id 업데이트

--- pages/api/UpdateProfile.ts
+++ pages/api/UpdateProfile.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
-import { redirect } from "next/dist/server/api-utils";
 
 const prisma = new PrismaClient();
 
@@ -9,42 +8,30 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method == "POST") {
-    try {
-      const { id, name, profileImg } = req.body;
+  const { user_id, name } = req.body;
 
-      const session = await getSession({ req });
+  const session = await getSession({ req });
 
-      if (!session?.user) {
-        return res.redirect("/Login/Login");
-      }
-
-      const userEmail: string | null = session?.user.email || "";
-      const userId: string = id;
-
-      const user = await prisma.user.update({
-        where: {
-          email: userEmail,
-        },
-        data: {
-          id: userId,
-          name: name as string,
-          profile_img: profileImg as string,
-        },
-      });
-
-      // const userId = await prisma.user.update({
-      //   where: {
-      //     email: userEmail,
-      //   },
-      //   data: {
-      //     id: id as string,
-      //   },
-      // });
-    } catch (error) {
-      console.log(error);
-    }
+  if (!session?.user) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
-  // req.statusCode = 200
+  try {
+    const userEmail: string | null = session?.user.email || "";
+
+    const user = await prisma.user.update({
+      where: {
+        email: userEmail,
+      },
+      data: {
+        user_id: user_id,
+        name: name,
+        // profile_img: profileImg,
+      },
+    });
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
 }
