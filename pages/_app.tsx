07e1240cc14ce4a3ef7e1e2d794d2683027d9520
Minminjamin <0429el@gmail.com>
2023-09-22 08:39:16 +0900

    fix : 파일 경로 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 // import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import SideBar from "@/components/Template/Sidebar/Sidebar";
+import SideBar from "@/components/Template/SideBar/SideBar";
 // import "main.modlue.css";
 import "../styles/globals.css";
 import styles from "../styles/Main.module.css";
